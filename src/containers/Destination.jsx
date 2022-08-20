493ef0c6841e90878c5fced74a3bff64804ff048
Emmanuel Maduakor <emma080355@gmail.com>
2022-08-21 00:42:17 +0100

    finishing up

--- src/containers/Destination.jsx
+++ src/containers/Destination.jsx
@@ -27,29 +27,34 @@ const Destination = () => {
   }
   return ( 
     <div className='text-white'>
-      <div className='uppercase font-Heading font text-center mb-6'>
+      <div className='uppercase font-Heading font text-center md:text-left md:ml-7 xl:ml-44 mb-6'>
         <span className='font-paragraph text-white/25 font-bold mr-2'>01</span>pick your destination
       </div>
-      <div className='flex flex-col items-center'>
-        <img src={planetImg} className='w-40 h-40 sm:w-52 sm:h-52' alt='planet' />
-        <div className='flex flex-col w-[90%]'>
-          <div className='flex justify-between w-3/5 mx-auto my-0 uppercase mt-5 font-paragraph sm:w-2/6'>
+      <div className='flex flex-col items-center xl:flex-row xl:w-[80%] xl:mx-auto xl:my-0 xl:justify-between'>
+        <img src={planetImg} className='w-40 h-40 sm:w-52 sm:h-52 xl:w-72 xl:h-72 xl:ml-20' alt='planet' />
+        <div className='flex flex-col w-[90%] xl:w-2/5'>
+          <div className='flex justify-between w-3/5 mx-auto my-0 uppercase mt-5 font-paragraph sm:w-2/6  xl:w-3/6 xl:mx-0'>
             {destinations.map((planet, i) => {
             return (
-               <div className={`${i === index ? 'border-b-4' : 'border-none'} cursor-pointer`} key={i} onClick={() => handleClick(planet, i)}>{planet.name}</div>
+               <div className={`${i === index ? 'border-b-4' : 'border-invisible'} 
+               cursor-pointer hover:border-white/50 hover:border-b-4`} key={i} 
+               onClick={() => handleClick(planet, i)}>{planet.name}
+               </div>
               )
             })}
           </div>
-          <div className='font-Heading text-6xl text-center mt-8 uppercase'>{destinations[index].name}</div>
-          <div className='text-center text-color-2'>{destinations[index].description}</div>
+          <div className='font-Heading text-6xl text-center mt-8 uppercase xl:text-left xl:text-8xl'>{destinations[index].name}</div>
+          <div className='text-center text-color-2 xl:w-[100%] xl:text-left xl:text-lg'>{destinations[index].description}</div>
           <div className='bg-[#383B4B] h-[1px] w-[100%] my-7' />
-          <div>
-            <div className='text-color-2 font-paragraph text-sm uppercase tracking-[.2em] text-center'>avg. distance</div>
-            <div className='font-Heading text-[28px] mt-2 uppercase text-center'>{destinations[index].distance}</div>
-          </div>
-          <div>
-            <div className='text-color-2 font-paragraph text-sm uppercase tracking-[.2em] text-center'>est. time travel</div>
-            <div className='font-Heading text-[28px] mt-2 uppercase text-center'>{destinations[index].travel}</div>
+          <div className='xl:flex xl:flex-row justify-between'>
+            <div>
+              <div className='text-color-2 font-paragraph text-sm uppercase tracking-[.2em] text-center'>avg. distance</div>
+              <div className='font-Heading text-[28px] mt-2 uppercase text-center'>{destinations[index].distance}</div>
+            </div>
+            <div>
+              <div className='text-color-2 font-paragraph text-sm uppercase tracking-[.2em] text-center'>est. time travel</div>
+              <div className='font-Heading text-[28px] mt-2 uppercase text-center'>{destinations[index].travel}</div>
+            </div>
           </div>
         </div>
       </div>
